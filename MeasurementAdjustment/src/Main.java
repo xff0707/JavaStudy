94fbe0bc439c36a4b794b5a69c9af99ff2fb5e71
xff <xff0707@outlook.com>
2020-09-19 23:56:06 +0800

    更新主函数

--- MeasurementAdjustment/src/Main.java
+++ MeasurementAdjustment/src/Main.java
@@ -1,78 +1,104 @@
+import gui.ChangeCoorsForm;
+import gui.IntersectionForm;
+import gui.TraverseForm;
+import gui.WelcomeFrom;
+
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+/**
+ * 主类，用于组织各窗体以成为一个整体
+ * @version V1.0
+ * @author xff07
+ */
 public class Main {
     public static void main(String[] args) {
+        //欢迎界面
+        WelcomeFrom w = new WelcomeFrom();
+
+
+
+
+        //坐标转换界面
+        ChangeCoorsForm c = new ChangeCoorsForm();
+        JButton changeBut = w.getChangeBut();
+        JButton backButc = c.getBackBut();
+
+        //事件监听器
+        lister(changeBut, w, c, backButc);
+
         /*
-        System.out.println(Math.cos(Math.toRadians(90)));
-        double d = Tools.DMSChange.DMSToRad(90.3030);
-        System.out.println(d);
-         */
-
-        JFrame jf = new JFrame();
-        jf.setTitle("支导线平差计算程序");
-        jf.setBounds(300,200,400,300);
-
-        /*
-        JMenuBar jmb = new JMenuBar();
-        JMenuItem jm1 = new JMenuItem("打开");
-        JMenuItem jm2 = new JMenuItem("计算");
-        JMenuItem jm3 = new JMenuItem("清除");
-        JMenuItem jm4 = new JMenuItem("关闭");
-        jmb.add(jm1);
-        jmb.add(jm2);
-        jmb.add(jm3);
-        jmb.add(jm4);
-        jf.setJMenuBar(jmb);
-
-
-
-        jm1.addMouseListener(new MouseAdapter() {
+        changeBut.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                System.out.println("sss");
-                JTextArea jt = ft.getTextArea1();
-
+                c.setVisible(true);
+                w.setVisible(false);
             }
         });
-
+        c.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                c.setVisible(false);
+                w.setVisible(true);
+            }
+        });
+        backButc.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                c.setVisible(false);
+                w.setVisible(true);
+            }
+        });
          */
 
 
+        //导线界面
+        TraverseForm s = new TraverseForm();
+        JButton spur = w.getspurTra();
+        JButton backButs = s.getBackBut();
+        lister(spur, w, s, backButs);
 
 
+        //交会定点界面
+        IntersectionForm i = new IntersectionForm();
+        JButton inter = w.getIntersectionBut();
+        JButton backButi = i.getBackBut();
+        lister(inter, w, i, backButi);
 
 
+    }
 
-
-
-
-
-        /*
-
-
-
-
-        /*
-        Line.Traverse t = new Line.Traverse(new Line.Point(100,100),new Line.Point(200,200));
-        t.add(Math.PI*3/2,100*Math.sqrt(2));
-        ArrayList<Line.Point> ps = t.getPoints();
-        //System.out.println(p);
-        for (Line.Point p : ps)
-        {
-            System.out.println(p);
-        }
-
-
-
-        /*
-        double a = Math.atan(-1);
-        System.out.println(a);
-        double a2 = Math.atan2(10,-10);
-        System.out.println(a2);
-        System.out.println(35*20);
-
-         */
+    /**
+     * 添加事件监听，以便实现界面的切换功能
+     * @param mainForm 主窗口
+     * @param form 对应另一窗体
+     * @param bt2 对应另一窗体的返回按钮
+     */
+    private static void lister(JButton bt,JFrame mainForm, JFrame form, JButton bt2)
+    {
+        bt.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                form.setVisible(true);
+                mainForm.setVisible(false);
+            }
+        });
+        form.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                form.setVisible(false);
+                mainForm.setVisible(true);
+            }
+        });
+        bt2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                form.setVisible(false);
+                mainForm.setVisible(true);
+            }
+        });
     }
 }
