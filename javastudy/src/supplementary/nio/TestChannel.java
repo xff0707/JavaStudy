bc56787d2def96f697cff4dc41f2e1b40908f6f2
xff <xff0707@outlook.com>
2020-09-23 23:41:32 +0800

    通道

--- javastudy/src/supplementary/nio/TestChannel.java
+++ javastudy/src/supplementary/nio/TestChannel.java
@@ -1,6 +1,13 @@
 package supplementary.nio;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /*
 一、通道(channel)：用于源节点与目标节点的连接。在Java NIO中负责缓冲区中数据的传输。channel本身不存储数据，因此需要配合缓冲区进行传输。
 
@@ -30,4 +37,86 @@ java.nio.channels.Channel接口
 
  */
 public class TestChannel {
+    public static void main(String[] args) {
+        test1();
+    }
+
+
+
+
+
+    //利用通道完成文件的复制（非直接缓冲区）
+    public static void test1()
+    {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        //①获取通道
+        FileChannel inChannel = null;
+        FileChannel outChannel = null;
+        try {
+            fis = new FileInputStream("myfile\\supfile\\吹雪.jpg");
+            fos = new FileOutputStream("myfile\\supfile\\吹雪2.jpg");
+
+            inChannel = fis.getChannel();
+            outChannel = fos.getChannel();
+
+            //②分配指定大小的缓冲区
+            ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+
+            //③将通道中的数据存入缓冲区中
+            while(inChannel.read(buf)!=-1)
+            {
+                buf.flip();//切换读取数据的模式
+                //④将缓冲区中的数据写入到通道中
+                outChannel.write(buf);
+                buf.clear();//清空缓冲区
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outChannel != null)
+            {
+                try {
+                    outChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (inChannel!=null)
+            {
+                try {
+                    inChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fis!=null)
+            {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos!=null)
+            {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+
+
+        }
+
+
+
+    }
+
 }
