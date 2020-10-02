ddd7453f3788ae001de484981eebaa0f9bb523d5
xff <xff0707@outlook.com>
2020-10-02 21:56:57 +0800

    自定义view，跟随手指的小兔子

---
+++ AndroidStudy/MyFirstAPP/myview/src/main/java/com/example/myview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myview;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.mylayout);
+        final RabbitView rabbit = new RabbitView(this);
+        rabbit.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                rabbit.bitmapX = event.getX();
+                rabbit.bitmapY = event.getY();
+                rabbit.invalidate();
+                return true;
+            }
+        });
+        frameLayout.addView(rabbit);
+    }
+}
